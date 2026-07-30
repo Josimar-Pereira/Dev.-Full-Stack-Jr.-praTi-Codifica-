e16ccad99c47c17402320f9045d9f44b03931069
Josimar Bitencourt Pereira <Josimabitencourt@hotmail.com>
2026-07-30 20:38:19 -0300

    Aprendendo Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Josimar";
+        int idade = 44;
+        System.out.println("Olá "+ nome+" "+idade+" "+"anos.");
+    }
+}
